71c53998309c88cf41972a0d465d4291382f0265
SeanHollen <37461272+SeanHollen@users.noreply.github.com>
2025-06-07 03:21:06 -0400

    SEO

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
-export const USE_CACHE = false;
+export const USE_CACHE = true;
 export const DEFAULT_CHART_WIDTH = 10000;
 export const CATEGORY_STRATEGY = "icons";
 export const SHOW_TOOLTIP_IMAGES = true; 
